4a92bdfd45b504ee80fda96e3773c978d1aec1a1
hfallows <hfallows@thoughtmachine.net>
2021-12-01 21:31:40 +0000

    2021 day 1 solution

--- 2021/day-1/main.go
+++ 2021/day-1/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+    "io"
     "log"
     "strconv"
+    "os"
+    "bufio"
+    "fmt"
 )
 
 const inputFile = "input.txt"
@@ -13,18 +17,72 @@ func main() {
         log.Fatalf("File does not exist: %s", inputFile)
     }
     defer file.Close()
-
+    
+    fmt.Printf("--- Part One ---\n")
+    
+    scanner := bufio.NewScanner(file)
     prevDepth := int(^uint(0) >> 1)
     dropCount := 0
 
     for scanner.Scan() {
         currDepth, err := strconv.Atoi(scanner.Text())
+        if err != nil {
+            log.Fatalf("File contains non-integer value: %s", scanner.Text())
+        }
         if currDepth > prevDepth {
-            dropCount += 1
+            dropCount++
         }
         prevDepth = currDepth
     }
+    
+    fmt.Printf("Answer: %d\n", dropCount)
 
-    log.Infof("Number of measurements larger than previous measurments: %i", dropCount)
+    fmt.Printf("--- Part Two ---\n")
+    
+    file.Seek(0, io.SeekStart)
+    scanner = bufio.NewScanner(file)
+    window := [4]int{}
+    firstSum := 0
+    secondSum := 0
+    ok := true
+    dropCount = 0
+
+    for i := 0; i <= 3; i++ {
+        ok = scanner.Scan()
+        if !ok {
+            log.Fatalf("Not enough inputs")
+        }
+        window[i], err = strconv.Atoi(scanner.Text())
+        if err != nil {
+            log.Fatalf("File contains non-integer value: %s", scanner.Text())
+        }
+        if i < 3 {
+            firstSum += window[i]
+        }
+        if i > 0 {
+            secondSum += window[i]
+        }
+    }
+
+    for ok {
+        if firstSum < secondSum {
+            dropCount++
+        }
+        ok = scanner.Scan()
+        if !ok {
+            break
+        }
+        firstSum -= window[0]
+        secondSum -= window[1]
+        copy(window[:3], window[1:])
+        window[3], err = strconv.Atoi(scanner.Text())
+        if err != nil {
+            log.Fatalf("File contains non-integer value: %s", scanner.Text())
+        }
+        firstSum += window[2]
+        secondSum += window[3]
+    }
+
+    fmt.Printf("Answer: %d\n", dropCount)
 }
 
